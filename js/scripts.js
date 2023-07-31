1b9a2b35585f00e31d4302ade8db09e15e4e09fc
SnazzyPulsar <136757777+SnazzyPulsar@users.noreply.github.com>
2023-07-30 19:22:31 -0500

    Removed all except alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,2 @@
 // adds pop up of 'Hello world'
 alert('Hello world');
-
-let favoriteFood = ramen;
-let dislikedFood = canned spinach;
-
-document.write (favoriteFood);
-document.write (dislikedFood);
