912a6018476cdf89c170dd74a31b2ad080673f29
SnazzyPulsar <136757777+SnazzyPulsar@users.noreply.github.com>
2023-07-31 22:00:19 -0500

    Changed variables
    Removed old variables and added an array for a list of pokemon for the exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,1 @@
-// adds pop up of 'Hello world'
-alert('Hello world');
-
-let favoriteFood ='ramen';
-document.write(favoriteFood);
+let pokemonList=[{name: 'Bulbasaur', height: 7, type: ['Grass', 'Poison'], evolutions: 2, megaevolution: true}, {name: 'Squirtle', height: 5, type: 'Water', evolutions: 2, megaevolution: true}, {name: 'Charmander', height: 6, type: 'Fire', evolutions: 2, megaevolution: true}, {name:'Caterpie', height: 3, type: 'Bug', evolutions: 2, megaevolution: false}, {name: 'Weedle', height: 3, type: ['Bug', 'Poison'], evolutions: 2, megaevolution: true}];
