c5c146b9663d46856066379ee1fc0cecd2dcaee4
SnazzyPulsar <136757777+SnazzyPulsar@users.noreply.github.com>
2023-08-01 20:25:46 -0500

    Cleaned format

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-let pokemonList=[{name: 'Bulbasaur', height: 7, type: ['Grass', 'Poison'], evolutions: 2, megaevolution: true}, {name: 'Squirtle', height: 5, type: 'Water', evolutions: 2, megaevolution: true}, {name: 'Charmander', height: 6, type: 'Fire', evolutions: 2, megaevolution: true}, {name:'Caterpie', height: 3, type: 'Bug', evolutions: 2, megaevolution: false}, {name: 'Weedle', height: 3, type: ['Bug', 'Poison'], evolutions: 2, megaevolution: true}];
+let pokemonList=[
+    {name: 'Bulbasaur', height: 7, type: ['Grass', 'Poison'], evolutions: 2, megaevolution: true}, 
+    {name: 'Squirtle', height: 5, type: 'Water', evolutions: 2, megaevolution: true}, 
+    {name: 'Charmander', height: 6, type: 'Fire', evolutions: 2, megaevolution: true}, 
+    {name:'Caterpie', height: 3, type: 'Bug', evolutions: 2, megaevolution: false}, 
+    {name: 'Weedle', height: 3, type: ['Bug', 'Poison'], evolutions: 2, megaevolution: true}
+];
